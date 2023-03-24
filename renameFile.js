e88170d62f2d09d242484f1626b80860e3feeaf9
CM Ngan <plusonecm@gmail.com>
2023-03-24 22:44:53 +0800

    update cms config, bulk image, video parser

---
+++ renameFile.js
@@ -0,0 +1,112 @@
+const sharp = require('sharp')
+const fs = require('fs').promises;
+
+// const sourceDir = __dirname + '/public/images/performance';
+const sourceDir = './public/images/performance';
+
+const getDirAndFile = (dir) => {
+  const arr = dir.split('/');
+  const file = arr.pop();
+  return [arr.join('/'), file];
+}
+
+const updateFiles = async (dir, fn) => {
+  const files = await fs.readdir(dir, { withFileTypes: true });
+  for (const file of files) {
+    const filePath = dir + '/' + file.name;
+    if (file.isDirectory()) {
+      await updateFiles(filePath, fn);
+    } else {
+      await fn(filePath)
+    }
+  }
+
+
+}
+
+const main = async () => {
+  // recursively read all files in the directory
+  // and rename them to lowercase
+  await updateFiles(sourceDir, async (filePath) => {
+
+    //#region resize images
+    try {
+
+      const ext2 = filePath.toLowerCase().split('.').pop();
+
+      if (!['jpg', 'png', 'jpeg', 'heic'].includes(ext2))
+        return;
+
+      const tempImage = filePath.replace(`.${ext2}`, `-temp.${ext2}`);
+
+      await fs.rename(filePath, tempImage);
+
+      await sharp(tempImage)
+        .resize(1920)
+        .toFile(filePath.replace(`.${ext2}`, '.jpg'))
+
+      await fs.unlink(tempImage);
+
+      return
+    } catch (error) {
+      console.log('=================');
+      console.log(filePath);
+      throw error;
+
+    }
+    //#endregion
+
+    const newFilePath = filePath
+      .toLowerCase()
+      .replace(/ /g, '_')
+      .replace(/’/g, '_')
+      .replace(/9_it_s_a_hand\./g, '9_it_s_a_hand')
+      .replace(/\.\./g, '.')
+      .replace(/__/g, '_')
+      .replace(/__/g, '_')
+      .replace('_strangers_in_the_garden_2-_black_milk_white_coal_', 'strangers_in_the_garden_2_black_milk_white_coal')
+      // .replace('2_i_m_fine.', '2_i_m_fine')
+      ;
+
+    const ext = filePath.toLowerCase().split('.').pop();
+
+    if (!['jpg', 'png', 'jpeg', 'docx', 'mov', 'mp4', 'heic'].includes(ext))
+      console.error(`Unknown file extension: ${ext} in ${filePath}`);
+
+    const [dir, file] = getDirAndFile(newFilePath);
+    if (dir.endsWith('-modified')) {
+      const newDir = dir.replace('-modified', '');
+      await fs.mkdir(newDir, { recursive: true });
+      await fs.rename(filePath, newDir + '/' + file);
+      console.count('count2');
+
+      return;
+    }
+
+    if (filePath !== newFilePath) {
+      if (ext === 'docx') {
+        await fs.unlink(filePath);
+      } else {
+        const [dir, file] = getDirAndFile(newFilePath);
+        const newDir = dir + '-modified';
+        await fs.mkdir(newDir, { recursive: true });
+        await fs.rename(filePath, newDir + '/' + file);
+      }
+
+      console.log(filePath);
+      console.log(newFilePath);
+      console.log('-----------------');
+      console.count('count');
+    }
+
+
+    // fs.rename(filePath, newFilePath, (err) => {
+    //   if (err) {
+    //     console.log(err);
+    //   }
+    // });
+  });
+
+}
+
+main();
--- src/utils/html-parser.tsx
+++ src/utils/html-parser.tsx
@@ -3,17 +3,39 @@ import { cn } from '@/styles/classnames'
 import parse, { type HTMLReactParserOptions, Element } from 'html-react-parser'
 import Image from 'next/image'
 
+// https://youtu.be/hlBtknFhRho
+const getVideoUrl = (url: string) => {
+  if (/youtu.be/.test(url)) {
+    const id = url.match(/[^/]+$/)
+    if (id && id[0]) return `https://www.youtube.com/embed/${id[0]}?autoplay=1`
+  }
+  return url
+}
+
 export const parseHtml = (html: string) => {
   const options: HTMLReactParserOptions = {
     replace: (domNode) => {
       if (!(domNode instanceof Element && domNode.attribs)) return
 
+      if (domNode.name === 'youtube') {
+        const url = getVideoUrl(domNode.attribs.src || '')
+
+        return (
+          <div className="relative h-0 w-full pb-[56.25%]">
+            <iframe
+              className="absolute top-0 left-0 h-full w-full"
+              src={url}
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            />
+          </div>
+        )
+      }
+
       if (domNode.name === 'slider') {
         const images =
           domNode.attribs.images?.split(',').map((v) => v.trim()) || []
         const className = domNode.attribs.class || ''
-        console.log(domNode.attribs.images, typeof domNode.attribs.images)
-        console.log(domNode.attribs.class)
         return (
           <Slider
             sliderClass="h-full"
