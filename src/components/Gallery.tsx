bd095c4937d8de64ef1511d44a634fbfca3ffd09
CM Ngan <plusonecm@gmail.com>
2023-03-26 21:58:11 +0800

    add home cms, fix gallery loading

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -13,22 +13,23 @@ const Gallery = ({ list }: propsT) => {
   return (
     <div className="container grid grid-cols-2 md:grid-cols-3">
       {list.map((image) => (
-        <div
+        <Link
+          href={image.slug}
           key={image.title}
           className="group relative col-span-1 h-[45vw] max-h-[45vw] md:h-[30vw] md:max-h-[30vw]"
         >
-          <Link href={image.slug}>
-            <Image
-              src={image.imageSrc}
-              alt={image.title}
-              className="h-full w-full object-cover"
-              fill
-            />
-            <div className="absolute bottom-0 top-0 left-0 right-0 flex items-end bg-black bg-opacity-40 p-4 group-hover:opacity-100 md:opacity-0">
-              <span className="text-xs text-white">{image.title}</span>
-            </div>
-          </Link>
-        </div>
+          <Image
+            src={image.imageSrc}
+            alt={image.title}
+            className="h-full w-full object-cover"
+            fill
+            sizes="(max-width: 768px) 50vw,
+                    33vw"
+          />
+          <div className="absolute bottom-0 top-0 left-0 right-0 flex items-end bg-black bg-opacity-40 p-4 group-hover:opacity-100 md:opacity-0">
+            <span className="text-xs text-white">{image.title}</span>
+          </div>
+        </Link>
       ))}
     </div>
   )
